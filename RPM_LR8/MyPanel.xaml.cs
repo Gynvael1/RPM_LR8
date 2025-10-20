7128034f351e83e48dec8fac97767f8155a32607
gynvael1 <savmal1357@gmail.com>
2025-10-20 20:19:17 +0300

    Добавьте файлы проекта.

---
+++ RPM_LR8/MyPanel.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RPM_LR8
+{
+    public partial class MyPanel : UserControl
+    {
+        public double StartX { get; set; }
+        public double StartY { get; set; }
+        public double StartSkew { get; set; }
+        public double StartScaleX { get; set; }
+        public double StartScaleY { get; set; }
+        public double StartOpacity { get; set; }
+
+        public MyPanel()
+        {
+            InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                ((TranslateTransform)((TransformGroup)Panel.RenderTransform).Children[0]).X = StartX;
+                ((TranslateTransform)((TransformGroup)Panel.RenderTransform).Children[0]).Y = StartY;
+                ((SkewTransform)((TransformGroup)Panel.RenderTransform).Children[1]).AngleY = StartSkew;
+                ((ScaleTransform)((TransformGroup)Panel.RenderTransform).Children[2]).ScaleX = StartScaleX;
+                ((ScaleTransform)((TransformGroup)Panel.RenderTransform).Children[2]).ScaleY = StartScaleY;
+                Panel.Opacity = StartOpacity;
+            };
+        }
+    }
+}
